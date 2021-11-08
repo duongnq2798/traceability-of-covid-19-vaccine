2e5d41e4cfa13d586f6f6ae8caeb2d53cbaae970
duongnq2798 <duongnq27.98@gmail.com>
2021-11-08 19:29:46 +0700

    hero

--- dissertation_backend/src/models/temperature.schema.ts
+++ dissertation_backend/src/models/temperature.schema.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose'
 export const TemperatureSchema = new mongoose.Schema({
     temperature: String,
     humidity: String,
+    sensorName: String,
     createdAt: {
         type: Date,
         default: Date.now,
--- dissertation_backend/src/types/temperature.type.ts
+++ dissertation_backend/src/types/temperature.type.ts
@@ -3,5 +3,6 @@ import { Document } from 'mongoose';
 export interface Temperature extends Document {
   temperature: string;
   humidity: string;
+  sensorName: string;
   createdAt: Date;
 }
